32dddb076424e46cebc2b2c8cc082df9a2fcd0b0
carlosdayton <daytonribeiro365@gmail.com>
2025-04-19 20:09:23 -0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-import static java.net.http.HttpClient.newHttpClient;
-
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
