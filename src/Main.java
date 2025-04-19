5913dd7f4dffa043cbabc039be08017930c47045
carlosdayton <daytonribeiro365@gmail.com>
2025-04-19 20:28:41 -0300

    Deixando a API privada

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,21 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        Dotenv dotenv = Dotenv.load();
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o nome do livro a ser buscado: ");
         String livro = sc.nextLine();
 
-        String api = "AIzaSyDig9n50BB--EdeGHe0f8jwKQgYIVJEiv4";
-        String url = "https://www.googleapis.com/books/v1/volumes?q=" + livro.replace(" ", "+") + "&key=" + api;
+        String apikey = dotenv.get("GOOGLE_BOOKS_API_KEY");
+        System.out.println("Minha chave: " + apikey);
+        String url = "https://www.googleapis.com/books/v1/volumes?q=" + livro.replace(" ", "+") + "&key=" + apikey;
 
         HttpClient cliente = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
